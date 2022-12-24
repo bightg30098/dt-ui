15dab77dc0db1df317bff6a63b9f7695f5f9b5d8
bightg30098 <bightg30098@gmail.com>
2022-12-24 18:15:00 +0800

    feat: make storybook works

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,18 +1,20 @@
+const svgrPlugin = require('vite-plugin-svgr');
+const tsconfigPaths = require('vite-tsconfig-paths');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-vite',
   },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+  features: {
+    storyStoreV7: true,
+  },
+  async viteFinal(config) {
+    return {
+      ...config,
+      plugins: config.plugins.concat(svgrPlugin(), tsconfigPaths.default()),
+    };
+  },
+};
