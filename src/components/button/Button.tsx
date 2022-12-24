0b02f75a470a503f9491c01a7d0a3143ac453dbb
bightg30098 <bightg30098@gmail.com>
2022-12-24 17:46:08 +0800

    feat: add tailwindcss

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,9 @@
+import type { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function Button({ children }: Props) {
+  return <button>{children}</button>;
+}
