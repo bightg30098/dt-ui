d329cdbbda6fb8ef847d888f98635dd75bd872b0
bightg30098 <bightg30098@gmail.com>
2022-12-26 16:48:57 +0800

    feat: npm publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgrPlugin from 'vite-plugin-svgr';
@@ -6,4 +8,11 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgrPlugin(), tsconfigPaths()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.ts'),
+      fileName: 'components',
+      formats: ['es', 'cjs'],
+    },
+  },
 });
