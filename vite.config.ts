20936f70c972ad2539cb378809f6ab06ab92d612
bightg30098 <bightg30098@gmail.com>
2022-12-26 17:23:21 +0800

    feat: add vite plugin dts

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,33 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgrPlugin from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgrPlugin(), tsconfigPaths()],
+  plugins: [
+    react(),
+    svgrPlugin(),
+    tsconfigPaths(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
+    sourcemap: true,
     lib: {
-      entry: path.resolve(__dirname, 'src/components/index.ts'),
-      fileName: 'components',
-      formats: ['es', 'cjs'],
+      name: 'DT-UI',
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'umd'],
+      fileName: (format) => `dt-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
     },
   },
 });
