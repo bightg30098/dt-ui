4e17724ddaab7ca588b2145740dbecb74db0fa88
bightg30098 <bightg30098@gmail.com>
2023-01-07 17:16:17 +0800

    build: add build config

--- vite.config.ts
+++ vite.config.ts
@@ -6,29 +6,40 @@ import svgrPlugin from 'vite-plugin-svgr'
 import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    svgrPlugin(),
-    dts({
-      insertTypesEntry: true,
-    }),
-  ],
-  build: {
-    sourcemap: true,
-    lib: {
-      name: 'DT-UI',
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      formats: ['es', 'umd'],
-      fileName: (format) => `dt-ui.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
+export default defineConfig(async ({ command, mode }) => {
+  // build configuration
+  if (command === 'build') {
+    const pkg = await require('./package.json')
+    return {
+      plugins: [
+        react(),
+        svgrPlugin(),
+        dts({
+          insertTypesEntry: true,
+        }),
+      ],
+      build: {
+        sourcemap: true,
+        lib: {
+          name: 'DT-UI',
+          entry: path.resolve(__dirname, 'src/index.ts'),
+          formats: ['es', 'umd'],
+          fileName: (format) => `dt-ui.${format}.js`,
+        },
+        rollupOptions: {
+          external: Object.keys(pkg.dependencies),
+          output: {
+            globals: {
+              react: 'React',
+            },
+          },
         },
       },
-    },
-  },
+    }
+  }
+
+  // serve configuration
+  return {
+    plugins: [react(), svgrPlugin()],
+  }
 })
