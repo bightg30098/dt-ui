c1036f924ddfefea78165ca95affe83eb010717c
bightg30098 <bightg30098@gmail.com>
2023-01-07 22:22:53 +0800

    build: fix style import

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { Button } from '@bightg30098/dt-ui'
+
+import '@bightg30098/dt-ui/style.css'
+
 export default function App() {
-  return <></>
+  return (
+    <div className="h-screen w-screen bg-black p-8">
+      <div className="space-x-4">
+        <Button variant="primary">Primary</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="unstyled" className="text-gray-50">
+          Unstyled
+        </Button>
+      </div>
+    </div>
+  )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/components/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    ...(process.env.NODE_ENV === 'development'
+      ? ['./src/**/*.{js,jsx,ts,tsx}']
+      : ['./src/components/**/*.{js,jsx,ts,tsx}']),
+  ],
   theme: {
     extend: {},
   },
