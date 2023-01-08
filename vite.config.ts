b5c27fe5e9ade93b51ac89d1fdc7ec0e6b8c6b4f
bightg30098 <bightg30098@gmail.com>
2023-01-08 22:02:52 +0800

    feat: update vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
 import path from 'path'
+import fs from 'fs'
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgrPlugin from 'vite-plugin-svgr'
-import dts from 'vite-plugin-dts'
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig(async ({ command, mode }) => {
+  // serve configuration
+  if (command === 'serve') {
+    return {
+      plugins: [react(), svgrPlugin()],
+    }
+  }
+
   // build configuration
   if (command === 'build') {
-    const pkg = await require('./package.json')
+    const dts = (await import('vite-plugin-dts')).default
+    const cssInjectedByJsPlugin = (await import('vite-plugin-css-injected-by-js')).default
+    const pkg = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'package.json')).toString())
     return {
       plugins: [
         react(),
@@ -40,10 +48,5 @@ export default defineConfig(async ({ command, mode }) => {
         },
       },
     }
-  }
-
-  // serve configuration
-  return {
-    plugins: [react(), svgrPlugin()],
   }
 })
