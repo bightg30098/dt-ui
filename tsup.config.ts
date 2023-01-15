66996dcb22d5b67c69d6aca7006e0d238805109a
bightg30098 <bightg30098@gmail.com>
2023-01-15 19:51:34 +0800

    feat: update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,16 +6,31 @@ import { defineConfig } from 'tsup'
 const pkg = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'package.json')).toString())
 
 export default defineConfig({
+  // your entry file, can also use regexp like
+  // ['src/index.ts', 'src/components/**/*.{ts,tsx}', '!src/components/**/*.stories.*']
+  // to include all ts,tsx under components but stories.
   entry: ['src/index.ts'],
   sourcemap: true,
   clean: true,
+
+  // exclude all dependencies specified in package.json
   external: Object.keys(pkg),
+
+  // auto generate types.d.ts
   dts: true,
+
+  // esm for import, cjs for require
   format: ['esm', 'cjs'],
   minify: true,
+
+  // make sure treeshake works
   treeshake: true,
+
   outDir: 'dist',
+
+  // onSuccess callback
   async onSuccess() {
+    // manual cleanup empty folders, because `clean` flag wont do this
     cleanupEmptyFolders(path.resolve('dist'))
   },
 })
